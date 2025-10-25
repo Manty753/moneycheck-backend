1d973fb5cde86295c6c6cb13974e8b9d5b6c43cf
Jakub Novak <kubanovak1561995@gmail.com>
2025-10-25 17:01:53 +0200

    second commit

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import job from "./config/cron.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 dotenv.config();
+if (process.env.NODE_ENV === "production") job.start();
 app.use(rateLimiter);
 app.use(express.json());
 
